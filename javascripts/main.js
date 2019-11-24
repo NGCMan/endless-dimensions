ab0432fc242f409a26fb52546fb63bbd75b76ade
e666666 <a0939712328@gmail.com>
2019-11-24 11:43:08 +0800

    ND redo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,66 +1,77 @@
-var antiMatter = new Decimal(10)
-let gameLoopIntervalId = 0
-class Dimension {
-  constructor(cost, multiplier) {
-    this.amount = new Decimal(0)
-    this.bought = new Decimal(0)
-    this.baseCost = cost
-    this.cost = cost
-    this.multiplier = multiplier
+function getDefaultPlayer() {
+  return {
+    antiMatter: new Decimal(10),
+    metaShift: 0,
+    metaShiftCost: new Decimal(1e75),
+    ndOwned: new Decimal(10),
+    ndAmounts: [null],
+    ndBoughts: [null],
+    ndCosts: [null],
+    ndMults: [null]
   }
+}
 
-  buy() {
-    if (antiMatter.gte(this.cost)) {
-      antiMatter = antiMatter.minus(this.cost)
-      this.amount = this.amount.plus(1)
-      this.bought = this.bought.plus(1)
-      if (this.bought.gte(10)) {
-        this.bought = new Decimal(0)
-        this.cost = this.cost.times(this.baseCost)
-        this.multiplier = this.multiplier.times(2)
-      }
+function resetND() {
+  resetValues(["ndAmounts", "ndBoughts", "ndCosts", "ndMults"])
+  for (let i = 1; i <= player.ndOwned; i++) {
+    player.ndAmounts.push(new Decimal(0))
+    player.ndBoughts.push(0)
+    player.ndCosts.push(Decimal.pow(10, i))
+    player.ndMults.push(new Decimal(1))
+  }
+}
+
+var player = getDefaultPlayer()
+resetND()
+let gameLoopIntervalId = 0
+
+var getNDCostScale = id => Decimal.pow(10, id)
+
+function buyND(id) {
+  if (player.antiMatter.gte(player.ndCosts[id])) {
+    player.antiMatter = player.antiMatter.minus(player.ndCosts[id])
+    player.ndAmounts[id] = player.ndAmounts[id].plus(1)
+    player.ndBoughts[id]++
+    if (player.ndBoughts[id] >= 10) {
+      player.ndBoughts[id] = 0
+      player.ndCosts[id] = player.ndCosts[id].times(getNDCostScale(id))
+      player.ndMults[id] = player.ndMults.times(2)
     }
   }
 }
-var firstDimension = new Dimension(new Decimal(10), new Decimal(1))
-var secondDimension = new Dimension(new Decimal(100), new Decimal(1))
-var thirdDimension = new Dimension(new Decimal(1000), new Decimal(1))
-var fourthDimension = new Dimension(new Decimal(10000), new Decimal(1))
-var fifthDimension = new Dimension(new Decimal(100000), new Decimal(1))
-var sixthDimension = new Dimension(new Decimal(1000000), new Decimal(1))
-var seventhDimension = new Dimension(new Decimal(10000000), new Decimal(1))
-var eighthDimension = new Dimension(new Decimal(100000000), new Decimal(1))
-var ninthDimension = new Dimension(new Decimal(1000000000), new Decimal(1))
-var tenthDimension = new Dimension(new Decimal(10000000000), new Decimal(1))
-var metaShift = 0
-var metaShiftCost = 1e75
 
-var getAMPerSec = _ => firstDimension.amount.times(firstDimension.multiplier)
-var getAntiGrindMulti = _ => tenthDimension.amount.div(15).plus(1)
+function resetValues(names) {
+  let reference = getDefaultPlayer()
+  names.forEach(function(name) {
+    _.set(player, name, _.get(reference, name))
+  })
+}
+
+var getAMPerSec = _ => player.ndAmounts[1].times(player.ndMults[1])
+var getAntiGrindMulti = _ => player.ndAmounts[10].div(15).plus(1)
+
+var getDimTitles = id => [null, "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"][id]
 
 function gameLoop() {
-  antiMatter = antiMatter.plus(
+  player.antiMatter = player.antiMatter.plus(
     getAMPerSec()
       .div(20)
       .times(getAntiGrindMulti())
   )
 
-  document.getElementById("AntimatterAmount").innerHTML = `You have ${shortenMoney(antiMatter)} antimatter.`
+  document.getElementById("AntimatterAmount").innerHTML = `You have ${shortenMoney(player.antiMatter)} antimatter.`
   document.getElementById("AntimatterMulti").innerHTML = `Your antimatter production multiplier is ${shorten(getAntiGrindMulti())}&times;`
 
-  let dims = [firstDimension, secondDimension, thirdDimension, fourthDimension, fifthDimension, sixthDimension, seventhDimension, eighthDimension, ninthDimension, tenthDimension]
-  let dim_titles = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth"]
-
-  for (let i = 0; i < 10; i++) {
-    document.getElementById(`dim${i}stats`).innerHTML = `You have ${shortenDimensions(dims[i].amount)} ${dim_titles[i]} dimensions. They have a ${dims[i].multiplier}&times; multiplier. ${dims[i].bought} bought. Cost: ${shorten(dims[i].cost)} antimatter.`
-    if (i < 9) dims[i].amount = dims[i].amount.plus(dims[i + 1].amount.times(dims[i + 1].multiplier).div(20 * (i + 1)))
+  for (let i = 1; i < 11; i++) {
+    document.getElementById(`dim${i}stats`).innerHTML = `You have ${shortenDimensions(player.ndAmounts[i])} ${getDimTitles(i)} dimensions. They have a ${player.ndMults[i]}&times; multiplier. ${player.ndBoughts[i]} bought. Cost: ${shorten(player.ndCosts[i])} antimatter.`
+    if (player.ndOwned.neq(i)) player.ndAmounts[i] = player.ndAmounts[i].plus(player.ndAmounts[i + 1].times(player.ndMults[i + 1]).div(20 * (i + 1)))
   }
 }
 function metaShift() {
-  if (antiMatter.times(metaShiftCost)) {
-    antiMatter = antiMatter.sub(metaShiftCost)
-    metaShift++
-    metaShiftCost = Decimal.pow(10, metaShiftCost)
+  if (player.antiMatter.gte(player.metaShiftCost)) {
+    player.antiMatter = player.antiMatter.sub(player.metaShiftCost)
+    player.metaShift++
+    player.metaShiftCost = Decimal.pow(10, player.metaShiftCost)
   }
 }
 
